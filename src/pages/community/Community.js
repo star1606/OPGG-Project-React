ffc849f9984cb883161f36e23928afffb9d2db96
Lee <you@example.com>
2020-08-25 11:54:02 +0900

    post list 완료

--- src/pages/community/Community.js
+++ src/pages/community/Community.js
@@ -3,9 +3,10 @@ import Header1 from "./../../include/Header1";
 import Footer2 from "./../../include/Footer2";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+
 const CommunityContainer = styled.div`
   margin: 0 auto;
-  background-color: aqua;
+  background-color: #5383e8;
   vertical-align: middle;
   justify-content: center;
   max-width: 1044px;
@@ -54,6 +55,7 @@ const SearchForm = styled.form`
     padding: 12px 62px 11px 12px;
     box-sizing: border-box;
     height: 40px;
+    margin-right: 8px;
   }
 
   .mainBtn {
@@ -61,7 +63,7 @@ const SearchForm = styled.form`
     top: 0;
     right: 0;
     margin-top: 5px;
-    margin-right: 6px;
+    margin-right: 15px;
     position: absolute;
     background: none;
   }
@@ -79,7 +81,7 @@ const ContentBox = styled.div`
   .content-header-wrap {
     display: flex;
     padding-top: 18px;
-    padding-bottom: 17px;
+    padding-bottom: 0px;
     justify-content: space-between;
   }
 
@@ -95,7 +97,7 @@ const ContentBox = styled.div`
     display: flex;
     margin-top: 0;
     border-top: 1px solid #ebeef1;
-    background: #f8f9fa;
+    background: #ffffff;
 
     line-height: 18px;
     font-size: 14px;
@@ -157,6 +159,81 @@ const ContentBox = styled.div`
     color: #98a0a7;
     padding-left: 8px;
   }
+
+  .sub-search-wrap {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    margin-right: 6px;
+    margin-bottom: 6px;
+  }
+
+  .sub-header-search__select {
+    float: left;
+    width: 122px;
+    padding: 9px 0 8px 15px;
+    /* -webkit-box-sizing: border-box; */
+    box-sizing: border-box;
+    border: 1px solid #ebeef1;
+    /* background: #fff; */
+    border-radius: 4px 0 0 4px;
+    line-height: 17px;
+    font-size: 14px;
+    color: #98a0a7;
+    background-image: url("/img/iconDropdown.png");
+    background-size: 24px;
+    background-position: top 5px right 5px;
+    background-repeat: no-repeat;
+    /* -webkit-appearance: none; */
+    -moz-appearance: none;
+    appearance: none;
+    outline: none;
+  }
+
+  .sub-header-search__input {
+    float: left;
+    border: none;
+    width: 200px;
+    /* -webkit-box-sizing: border-box; */
+    box-sizing: border-box;
+    padding: 10px 32px 9px 16px;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+    background-color: #ebeef1;
+    line-height: 17px;
+    font-size: 14px;
+  }
+
+  .sub-header-search__button {
+    float: left;
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin-top: 6px;
+    margin-right: 8px;
+  }
+
+  .sub-header-search__img {
+    width: 24px;
+    height: 24px;
+  }
+
+  .article-list-paging {
+    height: 64px;
+    background: #f8f9fa;
+  }
+
+  .article-list-paging__button {
+    line-height: 17px;
+    font-size: 14px;
+    color: #7b858e;
+    border-radius: 4px;
+    background-color: #fff;
+    border: 1px solid #dddfe4;
+    width: 77px;
+    height: 40px;
+    margin-top: 12px;
+  }
 `;
 
 const Community = () => {
@@ -194,6 +271,32 @@ const Community = () => {
                       alt="글쓰기"
                     />
                   </Link>
+                </div>
+              </div>
+
+              <div
+                className="content-header-sub"
+                style={{ height: "48px", position: "relative" }}
+              >
+                <div className="sub-search-wrap">
+                  <form className="sub-search">
+                    <select className="sub-header-search__select">
+                      <option>제목</option>
+                      <option>작성자</option>
+                    </select>
+                    <input
+                      tpye="text"
+                      className="sub-header-search__input"
+                      placeholder="검색"
+                    />
+                    <button className="sub-header-search__button">
+                      <img
+                        className="sub-header-search__img"
+                        src="img/iconSearch.png"
+                        alt="검색"
+                      />
+                    </button>
+                  </form>
                 </div>
               </div>
             </div>
@@ -401,6 +504,26 @@ const Community = () => {
                       <div className="article-list-author">ㅈ망겜ㅇㅈ</div>
                     </div>
                   </div>
+                </div>
+              </div>
+              <div>
+                <div className="article-list-paging">
+                  <ul>
+                    <li>
+                      <button className="article-list-paging__button">
+                        다음
+                        <img
+                          src="img/iconArrowRight.png"
+                          alt="다음"
+                          style={{
+                            width: "24px",
+                            height: "24px",
+                            verticalAlign: "middle",
+                          }}
+                        />
+                      </button>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
